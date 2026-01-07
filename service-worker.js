c7f449377c673bc468680bb944ca0e60da87c224
STAYVERYWICKED <stayverywicked7@gmail.com>
2026-01-07 12:12:25 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,11 @@
-
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("trivia-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "script.js"
+      ]);
+    })
+  );
+});
